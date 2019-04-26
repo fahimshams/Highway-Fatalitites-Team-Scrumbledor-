9fa263e407a5e968583cf81c7087b0453889fd80
natrakcha <37713509+natrakcha@users.noreply.github.com>
2019-04-26 00:22:59 -0500

    Add files via upload

--- pie/filter.js
+++ pie/filter.js
@@ -1,3 +1,4 @@
+
 // var rect = svg.append("rect")
 // .attr("x", 300)
 // .attr("y", 50)
@@ -85,19 +86,20 @@ class Filter
         //     {label: "DEC", active: true},]
  			};
 			
+      this.DayButtons = null;
       this.buildDayGrid();
       //this.buildMonthGrid();
     }
     
     buildMonthGrid()
     {
-    	let gridData = this.generateGridData(2, 6, 200, 40);
+    	let gridData = this.generateGridData(2, 6, {x: 200, y: 40}, {width: 25, height: 25});
     }
     
     buildDayGrid()
     {
     	// Generate the grid for the day buttons and inject day data.
-    	let gridData = this.generateGridData(1, 7, 300, 60);
+    	let gridData = this.generateGridData(1, 7, {x: 300, y: 60}, {width: 50, height: 50});
       let gridrow = gridData [0];
       for (let i = 0; i<7; i++){
           let entry = gridrow[i];
@@ -111,26 +113,51 @@ class Filter
           .attr("class","row");
        //
       let sThis = this;
+        
+      // Buttons // 
       let column = row.selectAll(".square")
           .data(function(d){
               return d;
           })
           .enter().append("rect")
-          .attr("class","square")
+          //.attr("class","square")
           .attr("x", function(d) {return d.x; })
           .attr("y", function(d) {return d.y; })
           .attr("width",function(d) {return d.width;})
           .attr("height",function(d) {return d.height;})
-          .style("fill","#fff")
+          .style("fill","green")
           .style("stroke","#222")
 					.on('click', function(d){
-            sThis.onDayClick(d);
+            sThis.handleFilterChanges(d);
+            sThis.setColorsForDayButtons();
             EventSystem.Instance.RaiseEvent("OnFilterChange", sThis.Filter);
         });
+        
+        // Text //
+      let text = row.selectAll(".square")
+          .data(function(d){
+              return d;
+          })
+          .enter().append("text")
+          .attr("x", function(d) {return d.x; })
+          .attr("y", function(d) {return d.y + (d.height / 2) + 5; })
+          .text(function(d) {
+          	return d.day.label;
+          })
+          .attr("font-family", "sans-serif")
+          .attr("font-size", "20px")
+          .attr("fill", "red")
+					.on('click', function(d){
+            sThis.handleFilterChanges(d);
+            sThis.setColorsForDayButtons();
+            EventSystem.Instance.RaiseEvent("OnFilterChange", sThis.Filter);
+        });
+        
+        this.DayButtons = row.selectAll("rect");
     }
     
     // Edits the day filter on click.
-    onDayClick(d)
+    handleFilterChanges(d)
     {
         //console.log(d.day.label);
       let dayName = d.day.label;
@@ -163,7 +190,7 @@ class Filter
         let checkingFilter = this.Filter.Days[i];
         let filterActive = checkingFilter.active;
 
-        if (!filterActive && checkingFilter.label != clickedFilter.label)
+        if (!filterActive)
         {
           clickedFilter.active = !clickedFilter.active;
           if (!clickedFilter.active)
@@ -195,13 +222,37 @@ class Filter
       }
     }
     
-    generateGridData(rowCount, columnCount, x, y)
+    setColorsForDayButtons()
+    {
+    	let sThis = this;
+    	this.DayButtons.style("fill", function(d) {
+      	let label = d.day.label;
+        for (let i = 0; i < sThis.Filter.Days.length; i++)
+        {
+        	let checkingFilter = sThis.Filter.Days[i];
+          if (checkingFilter.label == label)
+          {
+          	if (checkingFilter.active)
+            {
+            	return "green";
+            }
+            else
+            {
+            	return "red";
+            }
+          }
+        }
+      });
+    }
+    
+    // pos should be {x: #, y: #}, dimensions: {width: #, height: #}
+    generateGridData(rowCount, columnCount, pos, dimensions)
     {
       let data = new Array();
-      let xpos = x;
-      let ypos = y;
-      let width = 25;
-      let height = 25;
+      let xpos = pos.x;
+      let ypos = pos.y;
+      let width = dimensions.width;
+      let height = dimensions.height;
       let click = 0;
 
       for (let row = 0; row< rowCount; row++){
@@ -353,140 +404,6 @@ var column = row.selectAll(".square")
     if ((d.click)%4 == 3 ) {d3.select(this).style("fill","#838690");}
 })
 */
-
- 
-// /////////       /////////////////////////
-// ///////     HOURS      /////////
-// //////////////////////////////////
-
-// function gridHData(rowCount, columnCount){
-//     var data = new Array();
-//     var xpos = 200;
-//     var ypos =40;
-//     var width = 25;
-//     var height = 25;
-//     var click = 0;
- 
-//     for (var row = 0; row< rowCount; row++){
-//         data.push( new Array() );
- 
-//         for (var column = 0; column< columnCount; column++){
-//             data[row].push({
-//                 x: xpos,
-//                 y: ypos,
-//                 width: width,
-//                 height: height,
-//                 click: click,
-//               //  dayIndex: column,
-//             })
-         
-//             xpos += width; //imcrement the x pos
-//         }
- 
-//         xpos = 10;
-//         ypos += height;
-//     }
- 
-//     return data;
-// }
-
-// let HOURSArray = [
-//         {label: "1" },
-//         {label: "2" },
-//         {label: "3" },
-//         {label: "4"},
-//         {label: "5" },
-//         {label: "6" },
-//         {label: "7" },
-//         {label: "8" },
-//         {label: "9" },
-//         {label: "10" },
-//         {label: "11" },
-//         {label: "12" },
-// ];
-
-
-
-// var gridMData = gridMData(1, 12);
-//     let gridMrow = gridMData [0];
-//     for (let i = 0; i<12; i++){
-//         let entry = gridMrow[i];
-//         entry.day = MonthsArray[i];
-//     }
-//     console.log(gridMData);
- 
-// var grid = d3.select("#grid")
-//     .append("svg")
-//     .attr("width","1000px")
-//     .attr("height","250px");
- 
-// var row = grid.selectAll(".row")
-//     .data(gridMData)
-//     .enter().append("g")
-//     .attr("class","row")
-//  //
-// var column = row.selectAll(".square")
-//     .data(function(d){
-//         return d;
-//     })
-// .enter().append("rect")
-// .attr("class","square")
-// .attr("x", function(d) {return d.x; })
-// .attr("y", function(d) {return d.y; })
-// .attr("width",function(d) {return d.width;})
-// .attr("height",function(d) {return d.height;})
-// .style("fill","#fff")
-// .style("stroke","#222")
-
-// .on('click', function(d){
-//     console.log(d.dayData.MonthsArray.label);
-//     d.click ++;
-//     if ((d.click)%4 == 0 ) {d3.select(this).style("fill","#fff");}
-//     if ((d.click)%4 == 1 ) {d3.select(this).style("fill","#2c93E8");}
-//     if ((d.click)%4 == 2 ) {d3.select(this).style("fill","#F56C4E");}
-//     if ((d.click)%4 == 3 ) {d3.select(this).style("fill","#838690");}
-// })
-
-
-
-
-
-
-
- 
-
-
-
-/*    
-var square = svg.append("rect")
-    .attr("height", 50)
-    .attr("width", 50)
-    .attr("x", 10)
-    .attr("x", 10)
-    .attr("fill", "orange")
-    .on("mousedown", mouseDown)
-    .on("mouseup", mouseUp)
-    .on("click", mouseClick);
- 
-function mouseDown(){
-    console.log("mouseDown");
-}
- 
-function mouseUp(){
-    console.log("mouseup");
-}
- 
-function mouseClick(){
-    console.log("mouseClick");
-}
-
-
-
-
-*/
-
-
-
 
  
 // /////////       /////////////////////////
